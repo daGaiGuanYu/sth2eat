f82346cc8eb4c89b2d2e5fe344827d23d4881df4
gy <372301467@qq.com>
2021-04-01 12:18:44 +0800

    历史饭单

--- app/mp/page/history/index.js
+++ app/mp/page/history/index.js
@@ -1,5 +1,24 @@
-const { GFPage } = require('../../common/index')
+const { GFPage, showActionSheet } = require('../../common/index')
+const Model = require('../../db-util/model')
+const userApi = require('../../api/user')
 
+const model = new Model('list')
 const page = new GFPage()
+
+page.onLoad = async function(){
+  const userinfo = await userApi.getUserRecord()
+  const list = await Promise.all(
+    userinfo.list.map(id => model.findById(id))
+  )
+  this.setData({ list })
+}
+
+page.onTapItem = function(e){
+  showActionSheet([
+    ['详情', () => {}],
+    ['拷贝', () => {}],
+    ['设为当前饭单', () => {}]
+  ])
+}
 
 Page(page)
